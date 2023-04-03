6b811bdc94b96112788e9808f33068b9af22583e
Matthew Alvidrez <mateo75602@gmail.com>
2023-04-02 23:24:55 -0500

    finishing module 1 assignment

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 (function () {
     'use strict';
 
-    angular.module('LunchCheck', []);
+    angular.module('LunchCheck', [])
 
     .controller('MyFirstController', LunchCheckController);
 
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
         // this is where we are giong to be doing the operations needed
-        // 
+        // you need to call the ng-function from the html page
+        $scope.FeedMe = function () {
+            // this list is meant to take in all the variables from the txt box
+            var mylist = $scope.list;
+
+            // need to account for null
+            if (mylist === null){
+                $scope.message = "please enter something"
+                return;
+            }
+
+            // if the list has less than 3 and more than 0
+            else if(mylist.length <= 3 && mylist.length >= 0 ){
+                $scope.message = "Enjoy";
+                return;
+            }
+            // and if there are to many inputs
+            else{
+                $scope.message = "To much! "
+                return;
+            }
+
+            
+        }
+
 
     }
 })();

92d0da2d0c77661e32c604a3722ab9a9a42e351f
Matthew Alvidrez <mateo75602@gmail.com>
2023-04-03 09:39:24 -0500

    module 1 finished

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
     angular.module('LunchCheck', [])
 
-    .controller('MyFirstController', LunchCheckController);
+    .controller('LunchCheckController', LunchCheckController);
 
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
@@ -12,15 +12,17 @@
         $scope.FeedMe = function () {
             // this list is meant to take in all the variables from the txt box
             var mylist = $scope.list;
-
+            
             // need to account for null
             if (mylist === null){
                 $scope.message = "please enter something"
                 return;
             }
 
+            mylist = $scope.list.split(',');
+
             // if the list has less than 3 and more than 0
-            else if(mylist.length <= 3 && mylist.length >= 0 ){
+            if(mylist.length <= 3 && mylist.length > 0 ){
                 $scope.message = "Enjoy";
                 return;
             }
